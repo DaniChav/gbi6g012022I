500395d11ddc57cca52ccf9bf25680867aeb89b0
DaniChav <daniela.chavez@est.ikiam.edu.ec>
2022-05-25 03:28:29 -0500

    Tarea en clase y casa 3

---
+++ Tarea 3/netsize_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Chávez, Daniela"
+
+echo "Número de filas"
+for f in ../Saavedra2013/*.txt;
+do
+	cat $f 	|wc -l;
+done
+
+echo "Número de columnas"
+for f in ../Saavedra2013/*.txt;
+do
+	head -n 1 $f | tr -d " " | tr -d "\n" | wc  -c;
+done
