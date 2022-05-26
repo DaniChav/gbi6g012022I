924791fcd74c0f0ff66d299d2e07bdd190fa9c1a
DaniChav <daniela.chavez@est.ikiam.edu.ec>
2022-05-25 22:13:30 -0500

    Modificación tarea 3

--- Tarea 3/netsize_all.sh
+++ Tarea 3/netsize_all.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-echo "Escriba un script que imprima los números de las filas y las columnas para cada red" > netsize_all.txt
-echo "______________________________________________" >> netsize_all.txt
+echo "Escriba un script que imprima los números de las filas y las columnas para cada red." > netsize_all.txt
+echo "__________________________________" >> netsize_all.txt
 
-for file in $(ls *.txt); do wc -l $file >> netsize_all.txt; head -n1 $file|grep -o " "|wc -l >> netsize_all.txt; done
+echo "Daniela Chávez. Análisis de datos con bucle FOR" >> netsize_all.txt;
+
+for file in $(ls *.txt); 
+do 
+	wc -l $file >> netsize_all.txt; 
+	head -n1 $file|grep -o " "|wc -l >> netsize_all.txt; 
+done
